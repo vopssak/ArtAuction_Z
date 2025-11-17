922b9fd39ef905589d51f848b67d95434bab41c8
vopssak <vopssak@users.noreply.github.com>
2025-11-17 14:27:45 +0000

    Add factory-ape-frontend.java

---
+++ frontend/web/src/local-config/factory-ape-frontend.java
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/artauction_z/frontend/web/src/local-config/factory-ape-frontend.java
+// Generated at: 2025-12-03T23:45:44.331Z
+
